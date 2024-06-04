38bb27139fe98b707f3bdc36ecff1b96bff9b5a2
MikhinaNastya <mikhinanastya86@gmail.com>
2024-06-04 11:54:08 +0500

    Django51 ORM11

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,8 @@ from django.db import models
 class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f"{self.title} ({self.pk})"
@@ -10,6 +12,8 @@ class Post(models.Model):
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     text = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f"{self.text} ({self.pk})"
