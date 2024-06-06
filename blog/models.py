16e6b4b1e6ebdf46e04c45205aa35d1b6a791ca7
MikhinaNastya <mikhinanastya86@gmail.com>
2024-06-06 16:38:18 +0500

    Django521 ORM

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+
 
 class Category(models.Model):
     name = models.CharField(max_length=50)
@@ -8,7 +10,8 @@ class Category(models.Model):
 class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
-    categories_custom = models.ManyToManyField(Category, through="PostCategoriesCustom", related_name="posts")
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    categories = models.ManyToManyField(Category, related_name="posts")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
@@ -19,14 +22,11 @@ class Post(models.Model):
     def __str__(self):
         return f"{self.title} ({self.pk})"
 
-class PostCategoriesCustom(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    created_at = models.DateTimeField(auto_now=True)
 
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
     text = models.CharField(max_length=200)
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
